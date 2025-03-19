1b76c5f39b353e866c144db5e9a57795e91f3755
EmranHossen <emranhossen3075@gmail.com>
2025-03-20 03:18:29 +0600

    feat(domain): implement custom validation for Villa model

--- Src/ResortEase.Web/Controllers/VillaController.cs
+++ Src/ResortEase.Web/Controllers/VillaController.cs
@@ -28,6 +28,12 @@ namespace ResortEase.Web.Controllers
         [HttpPost]
         public IActionResult Create(Villa obj)
         {
+            if(obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("","The description cannot exactly match the Name");
+            }
+
+
             if(ModelState.IsValid)
             {
                 _db.Villas.Add(obj);
