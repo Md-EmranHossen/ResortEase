f9a980f7cfdf18d6ebcb6eedf82f259713ea8a61
EmranHossen <emranhossen3075@gmail.com>
2025-03-21 17:06:30 +0600

    feat(repo): complete Repository Pattern implementation

---
+++ Src/ResortEase.Infrastructure/Repository/VillaNumberRepository.cs
@@ -0,0 +1,26 @@
+﻿using ResortEase.Application.Common.Interfaces;
+using ResortEase.Domain.Entities;
+using ResortEase.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResortEase.Infrastructure.Repository
+{
+    public class VillaNumberRepository : Repository<VillaNumber>, IVillaNumberRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VillaNumberRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(VillaNumber entity)
+        {
+            _db.Update(entity);
+        }
+    }
+}
--- Src/ResortEase.Web/Controllers/VillaNumberController.cs
+++ Src/ResortEase.Web/Controllers/VillaNumberController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using ResortEase.Application.Common.Interfaces;
 using ResortEase.Domain.Entities;
 using ResortEase.Infrastructure.Data;
 using ResortEase.Web.ViewModels;
@@ -9,16 +10,17 @@ namespace ResortEase.Web.Controllers
 {
     public class VillaNumberController : Controller
     {
-        private readonly ApplicationDbContext _db;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public VillaNumberController(ApplicationDbContext db)
+        public VillaNumberController(IUnitOfWork unitOfWork)
         {
-            _db = db;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            var VillaNumbers = _db.VillaNumbers.Include(u => u.Villa).ToList();
+            var VillaNumbers = _unitOfWork.VillaNumber.GetAll(includeProperties: "Villa");
+
             return View(VillaNumbers);
         }
 
@@ -27,7 +29,7 @@ namespace ResortEase.Web.Controllers
         {
             VillaNumberVM villaNumberVM = new()
             {
-                VillaList = _db.Villas.ToList().Select(u => new SelectListItem
+                VillaList = _unitOfWork.Villa.GetAll().Select(u => new SelectListItem
                 {
                     Text = u.Name,
                     Value = u.Id.ToString()
@@ -41,12 +43,12 @@ namespace ResortEase.Web.Controllers
         {
             // ModelState.Remove("Villa");
 
-            bool roomNumberExist = _db.VillaNumbers.Any(u => u.Villa_Number == obj.VillaNumber.Villa_Number);
+            bool roomNumberExist = _unitOfWork.VillaNumber.Any(u => u.Villa_Number == obj.VillaNumber.Villa_Number);
 
             if (ModelState.IsValid && !roomNumberExist)
             {
-                _db.VillaNumbers.Add(obj.VillaNumber);
-                _db.SaveChanges();
+                _unitOfWork.VillaNumber.Add(obj.VillaNumber);
+                _unitOfWork.Save();
                 TempData["success"] = "The Villa Number has been created successfully.";
 
                 return RedirectToAction(nameof(Index));
@@ -57,7 +59,7 @@ namespace ResortEase.Web.Controllers
             }
 
 
-            obj.VillaList = _db.Villas.ToList().Select(u => new SelectListItem
+            obj.VillaList = _unitOfWork.Villa.GetAll().Select(u => new SelectListItem
             {
                 Text = u.Name,
                 Value = u.Id.ToString()
@@ -66,22 +68,17 @@ namespace ResortEase.Web.Controllers
             return View(obj);
         }
 
-
-
-
-
-
         public IActionResult Update(int villaNumberId)
         {
 
             VillaNumberVM villaNumberVM = new()
             {
-                VillaList = _db.Villas.ToList().Select(u => new SelectListItem
+                VillaList = _unitOfWork.Villa.GetAll().Select(u => new SelectListItem
                 {
                     Text = u.Name,
                     Value = u.Id.ToString()
                 }),
-                VillaNumber = _db.VillaNumbers.FirstOrDefault(u=>u.Villa_Number == villaNumberId)
+                VillaNumber = _unitOfWork.VillaNumber.Get(u => u.Villa_Number == villaNumberId)
             };
 
 
@@ -101,15 +98,15 @@ namespace ResortEase.Web.Controllers
 
             if (ModelState.IsValid)
             {
-                _db.VillaNumbers.Update(obj.VillaNumber);
-                _db.SaveChanges();
+                _unitOfWork.VillaNumber.Update(obj.VillaNumber);
+                _unitOfWork.Save();
                 TempData["success"] = "The Villa Number has been updated successfully.";
 
                 return RedirectToAction(nameof(Index));
             }
-            
 
-            obj.VillaList = _db.Villas.ToList().Select(u => new SelectListItem
+
+            obj.VillaList = _unitOfWork.Villa.GetAll().Select(u => new SelectListItem
             {
                 Text = u.Name,
                 Value = u.Id.ToString()
@@ -125,12 +122,12 @@ namespace ResortEase.Web.Controllers
 
             VillaNumberVM villaNumberVM = new()
             {
-                VillaList = _db.Villas.ToList().Select(u => new SelectListItem
+                VillaList = _unitOfWork.Villa.GetAll().Select(u => new SelectListItem
                 {
                     Text = u.Name,
                     Value = u.Id.ToString()
                 }),
-                VillaNumber = _db.VillaNumbers.FirstOrDefault(u => u.Villa_Number == villaNumberId)
+                VillaNumber = _unitOfWork.VillaNumber.Get(u => u.Villa_Number == villaNumberId)
             };
 
 
@@ -146,13 +143,13 @@ namespace ResortEase.Web.Controllers
         public IActionResult Delete(VillaNumberVM villaNumberVM)
         {
 
-            VillaNumber? objFromDb = _db.VillaNumbers
-                .FirstOrDefault(u => u.Villa_Number == villaNumberVM.VillaNumber.Villa_Number);
+            VillaNumber? objFromDb = _unitOfWork.VillaNumber
+                .Get(u => u.Villa_Number == villaNumberVM.VillaNumber.Villa_Number);
 
             if (objFromDb is not null)
             {
-                _db.VillaNumbers.Remove(objFromDb);
-                _db.SaveChanges();
+                _unitOfWork.VillaNumber.Remove(objFromDb);
+                _unitOfWork.Save();
                 TempData["success"] = "The Villa Number has been deleted successfully.";
 
                 return RedirectToAction(nameof(Index));
