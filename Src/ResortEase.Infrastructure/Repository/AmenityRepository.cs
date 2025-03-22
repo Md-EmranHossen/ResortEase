8d3b7fa4262256da6cf7bbf45c4efd30613a0867
EmranHossen <emranhossen3075@gmail.com>
2025-03-22 12:38:41 +0600

    feat(amenity): add amenity model and integration with villa

---
+++ Src/ResortEase.Infrastructure/Repository/AmenityRepository.cs
@@ -0,0 +1,28 @@
+﻿using ResortEase.Application.Common.Interfaces;
+using ResortEase.Domain.Entities;
+using ResortEase.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResortEase.Infrastructure.Repository
+{
+    public class AmenityRepository : Repository<Amenity>, IAmenityRepository
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public AmenityRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Amenity entity)
+        {
+            _db.Update(entity);
+        }
+    }
+}
